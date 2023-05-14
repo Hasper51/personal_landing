af1f5fc0c31f35fc13f21bb8d6dca9ec8004abe1
Hasper15 <ainur.vahitov2002@gmail.com>
2023-05-15 01:46:03 +0300

    add page cartoons

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+const modalWindow = document.querySelector('.modal');
+const buttonsModal = document.querySelectorAll('.cartoon_preview');
+let modalInner = document.querySelector('.modal_inner');
+
+buttonsModal.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        modalWindow.classList.add('active')
+        let video = `<div class="player"><video id="my_video" onclick="this[this.paused ? 'play' : 'pause']()" controls width="100%" height="auto" poster="../cartoons/posters/${index+1}.png" autoplay preload="none"><source src="../cartoons/media/${index+1}.webm" type="video/webm"></video></div>`
+        modalWindow.innerHTML = video
+        // modalInner.insertAdjacentHTML('afterBegin', video);
+        // modalInner.appendChild = `<video controls width="100%" height="auto" poster="../cartoons/posters/${index+1}.png" preload="none"><source src="../cartoons/media/${index+1}.webm" type="video/webm"></video>`
+
+        
+    })
+})
+
+modalWindow.addEventListener('click', () => {
+    modalWindow.classList.remove('active')
+    document.querySelector('#my_video').pause()
+})
